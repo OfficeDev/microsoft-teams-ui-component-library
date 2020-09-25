b4e458f8a8ee813bd5cbfb791062d8aed07dbabd
Will Shown <v-wishow@microsoft.com>
2020-09-25 16:28:23 -0700

    Export built JS and CSS.

--- just.config.ts
+++ just.config.ts
@@ -1,10 +1,20 @@
-import { task, tscTask, jestTask, eslintTask } from "just-scripts";
+import { task, series, jestTask, tscTask, eslintTask } from "just-scripts";
+import cpx from "cpx";
+import util from "util";
 import {
   startStorybookTask,
   buildStorybookTask,
 } from "./scripts/tasks/storybook";
 
-task("build", tscTask());
+const copy = util.promisify(cpx.copy);
+
+task("build:tsc", tscTask());
+task("build:cp", async function () {
+  return await copy("./src/components/**/*.css", "./lib/components");
+});
+
+task("build", series("build:cp", "build:tsc"));
+
 task("test", jestTask());
 task("start", startStorybookTask());
 task("build:storybook", buildStorybookTask());
