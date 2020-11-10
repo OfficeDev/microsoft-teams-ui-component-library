ed90f584365584dc76fc3360e107073d1614715a
Will Shown <v-wishow@microsoft.com>
2020-11-09 16:55:56 -0800

    Create stub Form.

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export interface IFormProps {}
+
+export const Form = (props: IFormProps) => {
+  return <span>Hello.</span>;
+};
---
+++ src/stories/Form.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "../components/Form";
+
+export default {
+  title: "Composites/Form",
+  component: Form,
+};
+
+const formKnobGroupID = "Form";
+
+export const KitchenSink = () => {
+  return <Form />;
+};
