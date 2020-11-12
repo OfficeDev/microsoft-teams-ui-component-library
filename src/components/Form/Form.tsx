7537afe8271be0e4ebc161098175ab629280342e
Will Shown <v-wishow@microsoft.com>
2020-11-11 16:20:24 -0800

    Add border when sticky.

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,5 +1,14 @@
-import React from "react";
-import { TTextObject } from "../../translations";
+import React, { PropsWithChildren } from "react";
+
+import {
+  Box,
+  Button,
+  ProviderConsumer as FluentUIThemeConsumer,
+  SiteVariablesPrepared,
+  Text,
+} from "@fluentui/react-northstar";
+
+import { getText, TTextObject, TTranslations } from "../../translations";
 
 export interface IInputGroupBase {
   title?: TTextObject;
@@ -62,7 +71,7 @@ export type TInputGroup =
 export interface ISection {
   title?: TTextObject;
   preface?: TTextObject;
-  inputGroups: TInputGroup[];
+  inputGroups?: TInputGroup[];
 }
 
 export type TFormErrors = { [groupId: string]: TTextObject };
@@ -71,8 +80,133 @@ export interface IFormProps {
   headerSection?: ISection;
   sections: ISection[];
   errors?: TFormErrors;
+  submit: TTextObject;
+  cancel?: TTextObject;
 }
 
-export const Form = (props: IFormProps) => {
-  return <span>Hello.</span>;
+interface IFormSectionProps {
+  section: ISection;
+  header?: false;
+  errors?: TFormErrors;
+  t: TTranslations;
+}
+
+interface IFormHeaderSectionProps {
+  section: ISection;
+  header: true;
+  errors?: TFormErrors;
+  t: TTranslations;
+}
+
+const MaxWidth = ({ children, styles }: PropsWithChildren<any>) => (
+  <Box
+    styles={{
+      margin: "0 auto",
+      maxWidth: "29.75rem",
+      padding: "1.25rem",
+      ...styles,
+    }}
+  >
+    {children}
+  </Box>
+);
+
+const FormSection = ({
+  errors,
+  header,
+  section,
+  t,
+}: IFormSectionProps | IFormHeaderSectionProps) => {
+  return (
+    <>
+      {section.title && (
+        <Text
+          as="p"
+          weight={header ? "bold" : "semibold"}
+          {...(header ? { size: "large" } : {})}
+        >
+          {getText(t.locale, section.title)}
+        </Text>
+      )}
+      {section.preface && (
+        <Text as="p">{getText(t.locale, section.preface)}</Text>
+      )}
+    </>
+  );
+};
+
+export const Form = ({
+  cancel,
+  errors,
+  headerSection,
+  sections,
+  submit,
+}: IFormProps) => {
+  return (
+    <FluentUIThemeConsumer
+      render={(globalTheme) => {
+        const { t } = globalTheme.siteVariables;
+        return (
+          <>
+            <MaxWidth>
+              {headerSection && (
+                <FormSection
+                  header
+                  section={headerSection}
+                  {...{ t, errors }}
+                />
+              )}
+              {sections.map((section, si) => {
+                return (
+                  <FormSection
+                    {...{ section, t, errors }}
+                    key={`Form__Section_${si}`}
+                  />
+                );
+              })}
+            </MaxWidth>
+            <Box
+              variables={({ colorScheme }: SiteVariablesPrepared) => ({
+                backgroundColor: colorScheme.default.background,
+              })}
+              styles={{
+                height: "1px",
+                position: "absolute",
+                width: "100%",
+                zIndex: 1,
+              }}
+            />
+            <Box
+              variables={({ colorScheme }: SiteVariablesPrepared) => ({
+                backgroundColor: colorScheme.default.border1,
+              })}
+              styles={{ height: "1px", position: "sticky", bottom: "4.5rem" }}
+            />
+            <Box
+              variables={({ colorScheme }: SiteVariablesPrepared) => ({
+                backgroundColor: colorScheme.default.background,
+              })}
+              styles={{
+                position: "sticky",
+                bottom: 0,
+                height: "4.5rem",
+                zIndex: 2,
+              }}
+            >
+              <MaxWidth
+                styles={{ display: "flex", justifyContent: "flex-end" }}
+              >
+                {cancel && (
+                  <Button styles={{ marginRight: ".5rem" }}>
+                    {getText(t.locale, cancel)}
+                  </Button>
+                )}
+                <Button primary>{getText(t.locale, submit)}</Button>
+              </MaxWidth>
+            </Box>
+          </>
+        );
+      }}
+    />
+  );
 };
--- src/stories/Form.stories.tsx
+++ src/stories/Form.stories.tsx
@@ -1,4 +1,13 @@
 import React from "react";
+import fakerEN from "faker/locale/en_US";
+import fakerFA from "faker/locale/fa";
+
+const fake = (template: string) => {
+  return { "en-US": fakerEN.fake(template), fa: fakerFA.fake(template) };
+};
+
+import { object } from "@storybook/addon-knobs";
+
 import Form from "../components/Form";
 
 export default {
@@ -8,6 +17,32 @@ export default {
 
 const formKnobGroupID = "Form";
 
+const formConfig = {
+  submit: {
+    en: "Okay",
+    fa: "تایید",
+  },
+  cancel: {
+    en: "Cancel",
+    fa: "لغو",
+  },
+  headerSection: {
+    title: fake("{{company.catchPhrase}}"),
+    preface: fake("{{lorem.sentences}}"),
+  },
+  sections: [
+    {
+      title: (() => {
+        const title = fake("{{company.bs}}");
+        title["en-US"] =
+          title["en-US"].charAt(0).toUpperCase() + title["en-US"].slice(1);
+        return title;
+      })(),
+      ...(Math.random() > 0.5 && { preface: fake("{{lorem.sentence}}") }),
+    },
+  ],
+};
+
 export const KitchenSink = () => {
-  return <Form />;
+  return <Form {...object("Configuration", formConfig, formKnobGroupID)} />;
 };
