fb7d0b26809aad13f09c2e64bdfb23226eb2b49c
Will Shown <v-wishow@microsoft.com>
2021-01-14 12:56:08 -0800

    (Revert T10s provider)

--- src/translations/T10sProvider.tsx
+++
@@ -1,6 +0,0 @@
-import React, { createContext } from "react";
-import { TTranslations } from "./";
-
-const T10sContext = createContext({ locale: "en-US" } as TTranslations);
-
-export default T10sContext;
