0908d4d45e580435a864d3a71d16f6f9f352997a
David Zukowski <david@zuko.me>
2021-01-14 22:12:01 -0600

    Remove need to copy ./types folder

--- just.config.ts
+++ just.config.ts
@@ -1,19 +1,11 @@
 import { task, series, jestTask, tscTask, eslintTask } from "just-scripts";
-import cpx from "cpx";
-import util from "util";
 import {
   startStorybookTask,
   buildStorybookTask,
 } from "./scripts/tasks/storybook";
 
-const copy = util.promisify(cpx.copy);
-
 task("build:tsc", tscTask());
-task("build:cp", async function () {
-  await copy("./src/types/*", "./lib/types");
-});
-
-task("build", series("build:cp", "build:tsc"));
+task("build", series("build:tsc"));
 
 task("test", jestTask());
 task("start", startStorybookTask());
