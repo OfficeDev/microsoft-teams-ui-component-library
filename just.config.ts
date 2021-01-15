a36e076de55993f2230dc1da6b7dc9b40c0b8dc1
David Zukowski <david@zuko.me>
2021-01-15 08:44:32 -0600

    Clean output dir (lib) before each build

--- just.config.ts
+++ just.config.ts
@@ -1,11 +1,16 @@
+import * as util from "util";
+import _rimraf from "rimraf";
 import { task, series, jestTask, tscTask, eslintTask } from "just-scripts";
 import {
   startStorybookTask,
   buildStorybookTask,
 } from "./scripts/tasks/storybook";
 
+const rimraf = util.promisify(_rimraf as any);
+
+task("clean", () => rimraf("lib"));
 task("build:tsc", tscTask());
-task("build", series("build:tsc"));
+task("build", series("clean", "build:tsc"));
 
 task("test", jestTask());
 task("start", startStorybookTask());
